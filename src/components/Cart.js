bc4c38762513bf2d14cf033d8d83fdaa6f062803
Guerman <guerman.rouban@gmail.com>
2021-11-08 15:48:17 +0100

    créer un composant PlantItem qui sera un composant enfant de ShoppingList.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,20 @@
 import '../styles/Cart.css'
 
 function Cart() {
-	const priceMonstera = 8;
-	const priceLierre = 10;
-	const priceBouquet = 15;
+	const monsteraPrice = 8
+	const ivyPrice = 10
+	const flowerPrice = 15
 	return (
-		<div className="lmj-cart">
+		<div className='lmj-cart'>
 			<h2>Panier</h2>
 			<ul>
-				<li>Monstera : {priceMonstera} €</li>
-				<li>Lierre: {priceLierre} €</li>
-				<li>Bouquet: {priceBouquet} €</li>
+				<li>Monstera : {monsteraPrice}€</li>
+				<li>Lierre : {ivyPrice}€</li>
+				<li>Fleurs : {flowerPrice}€</li>
 			</ul>
-			Total: {priceMonstera + priceLierre + priceBouquet} €
+			Total : {monsteraPrice + ivyPrice + flowerPrice}€
 		</div>
-	);
+	)
 }
 
 export default Cart
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,6 @@
-
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
-
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -17,13 +16,15 @@ function ShoppingList() {
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul className="lmj-plant-list">
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.isBestSale && <span>🥰</span>}
-						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-					</li>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
@@ -31,4 +32,3 @@ function ShoppingList() {
 }
 
 export default ShoppingList
-
