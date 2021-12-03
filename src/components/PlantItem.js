54c1e3e80a9845ca9c3d097c592b04b55873176f
Guerman <guerman.rouban@gmail.com>
2021-12-03 13:23:55 +0100

    ajout Categories dans le useState

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,30 +1,22 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light, price }) {
 	return (
-		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
 				<CareScale careType='water' scaleValue={water} />
 				<CareScale careType='light' scaleValue={light} />
 			</div>
-			<form onSubmit={handleSubmit}>
-				<input type='text' name='my_input' defaultValue='Tapez votre texte' />
-				<button type='submit'>Entrer</button>
-			</form>
 		</li>
 	)
 }
 
-function handleClick(e) {
-	console.log('✨ Ceci est mon event :', e)
-}
-
-
-function handleSubmit(e) {
-	e.preventDefault()
-	alert(e.target['my_input'].value)
-}
 export default PlantItem
