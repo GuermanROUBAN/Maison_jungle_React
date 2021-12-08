7555230a48c7c682f6b1e3c6102a4f676d3d2822
Guerman <guerman.rouban@gmail.com>
2021-12-08 09:51:12 +0100

    Ajout de styles prices et centrage shopping-list

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,8 @@
 // useState permet au composant Cart d'etre stateful 
 // => les données sont sauvgardées dans le panier
 // ==> on peut re-render autant de fois qu'on veut les données ne seront pas perdues.
-import { useState } from 'react'
 
+import { useState, useEffect } from 'react'
 // useState est un hook
 // => il permet d'ajouter le state local React à des composants fonctions
 // ==> un hook est une fonction qui se permet de se brancher sur des fonctionnalités React
@@ -11,7 +11,9 @@ import { useState } from 'react'
 import '../styles/Cart.css'
 
 
+
 function Cart({ cart, updateCart }) {
+	
 	const [isOpen, setIsOpen] = useState(true)
 	const total = cart.reduce(
 		(acc, plantType) => acc + plantType.amount * plantType.price,
@@ -26,8 +28,11 @@ function Cart({ cart, updateCart }) {
 	// const cartState = useState(0) ==> il faut préciser une valeur dans le state sinon elle est undefined
 	// const cart = cartState[0] ==> cart est la valeur actuelle
 	// const updateCart = cartState[1] ==> fonction qui permet de le modifier 
-
-	return isOpen ? (
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+  }, [total])
+	return isOpen ?
+	(
 		<div className='lmj-cart'>
 			<button
 				className='lmj-cart-toggle-button'
